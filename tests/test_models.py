ead50dcd0759f8a56c065d778d80380e1c994bdd
Shouvik <107673645+shouviknas@users.noreply.github.com>
2024-07-29 17:36:07 +0530

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -161,6 +161,19 @@ class TestProductModel(unittest.TestCase):
         products = Product.all()
         self.assertEqual(len(products), 5)
 
+#Test Case to Find a Product by Name
+     def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+            
     #Test Case to Find a Product by Availability
 
       def test_find_by_availability(self):
